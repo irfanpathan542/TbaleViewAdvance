3ca2f08ff0ff53881433818ce3729428570f9a1e
iFlame <iflame@Administrators-iMac.local>
2017-05-04 09:10:15 +0530

    Initial Commit

---
+++ Swift-TableView-Example/RecipesTableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  RecipesTableViewController.swift
+//  Swift-TableView-Example
+//
+//  Created by Bilal Arslan on 28/02/16.
+//  Copyright © 2016 Bilal ARSLAN. All rights reserved.
+//
+
+import UIKit
+
+struct Recipe {
+    let name: String
+    let thumbnails: String
+    let prepTime: String
+}
+
+class RecipesTableViewController: UITableViewController {
+    var recipes = [Recipe]()
+    let identifier: String = "tableCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: self, action: nil)
+        navigationItem.title = "Recipes"
+        initializeTheRecipes()
+    }
+    
+    func initializeTheRecipes() {
+        self.recipes = [Recipe(name: "Egg Benedict", thumbnails: "egg_benedict.jpg", prepTime: "1 hour"),
+                        Recipe(name: "Mushroom Risotto", thumbnails: "mushroom_risotto.jpg", prepTime: "30 min"),
+                        Recipe(name: "Full Breakfast", thumbnails: "full_breakfast.jpg", prepTime: "25 min"),
+                        Recipe(name: "Hamburger", thumbnails: "hamburger.jpg", prepTime: "15 min"),
+                        Recipe(name: "Ham and Egg Sandwich", thumbnails: "ham_and_egg_sandwich.jpg", prepTime: "20 min"),
+                        Recipe(name: "Creme Brelee", thumbnails: "creme_brelee.jpg", prepTime: "10 min"),
+                        Recipe(name: "White Chocolate Donut", thumbnails: "white_chocolate_donut.jpg", prepTime: "1 hour"),
+                        Recipe(name: "Starbucks Coffee", thumbnails: "starbucks_coffee.jpg", prepTime: "2 hour"),
+                        Recipe(name: "Vegetable Curry", thumbnails: "vegetable_curry.jpg", prepTime: "50 min"),
+                        Recipe(name: "Instant Noodle with Egg", thumbnails: "instant_noodle_with_egg.jpg", prepTime: "65 min"),
+                        Recipe(name: "Noodle with BBQ Pork", thumbnails: "noodle_with_bbq_pork.jpg", prepTime: "40 min"),
+                        Recipe(name: "Japanese Noodle with Pork", thumbnails: "japanese_noodle_with_pork.jpg", prepTime: "30 min"),
+                        Recipe(name: "Green Tea", thumbnails: "green_tea.jpg", prepTime: "25 min"),
+                        Recipe(name: "Thai Shrimp Cake", thumbnails: "thai_shrimp_cake.jpg", prepTime: "1.5 hour"),
+                        Recipe(name: "Angry Birds Cake", thumbnails: "angry_birds_cake.jpg", prepTime: "2 hour"),
+                        Recipe(name: "Ham and Cheese Panini", thumbnails: "ham_and_cheese_panini.jpg", prepTime: "35 min")]
+        
+        self.tableView.reloadData()
+    }
+    
+    // MARK: UITableView DataSource
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TableCell! = tableView.dequeueReusableCell(withIdentifier: identifier) as? TableCell
+        cell.configurateTheCell(recipes[indexPath.row])
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            recipes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .bottom)
+        }
+    }
+    
+    // MARK: Segue Method
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "recipeDetail" {
+            let indexPath = self.tableView!.indexPathForSelectedRow
+            let destinationViewController: DetailViewController = segue.destination as! DetailViewController
+            destinationViewController.recipe = recipes[indexPath!.row]
+        }
+    }
+}
---
+++ Swift-TableView-Example/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Swift-TableView-Example
+//
+//  Created by Bilal ARSLAN on 11/10/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
